92c3b31a14283d312a0b6d642c50ba7d5048e2c0
Kabir4700 <homayonk47@gmail.com>
2024-02-14 08:59:04 +0800

    add file

--- scripts/aplha-clash.js
+++ scripts/aplha-clash.js
@@ -1,12 +1,12 @@
 // function play(params) {
 // // step-1: hide the home sceen . to hide the screen add the class hidden to home screen
-// const homeSection = document.getElementById('home-screen')
-// homeSection.classList.add('hidden')
+const homeSection = document.getElementById('home-screen')
+homeSection.classList.add('hidden')
 // // console.log(homeSection.classList);
 // // show the playground
 
-// const playgroundSection = document.getElementById('play-ground');
-// playgroundSection.classList.remove('hidden');
+const playgroundSection = document.getElementById('play-ground');
+playgroundSection.classList.remove('hidden');
 // // console.log('playgroundSection.classlist');
 
 // }
