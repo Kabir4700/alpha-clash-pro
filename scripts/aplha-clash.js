e974119b5fa25a130225bc5d54e5246948c14036
Kabir4700 <homayonk47@gmail.com>
2024-02-15 03:19:03 +0800

    add code

--- scripts/aplha-clash.js
+++ scripts/aplha-clash.js
@@ -1,12 +1,37 @@
-// function play(params) {
-// // step-1: hide the home sceen . to hide the screen add the class hidden to home screen
-const homeSection = document.getElementById('home-screen')
-homeSection.classList.add('hidden')
+// // function play(params) {
+// // // step-1: hide the home sceen . to hide the screen add the class hidden to home screen
+// const homeSection = document.getElementById('home-screen')
+// homeSection.classList.add('hidden')
 // // console.log(homeSection.classList);
 // // show the playground
 
-const playgroundSection = document.getElementById('play-ground');
-playgroundSection.classList.remove('hidden');
+// const playgroundSection = document.getElementById('play-ground');
+// playgroundSection.classList.remove('hidden');
 // // console.log('playgroundSection.classlist');
 
-// }
+// // }
+
+function continueGame(){
+    // step 1  generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet',alphabet);
+
+    // randomly generate alphabet
+// get an random index between 0-25
+// get an random index between 0-25
+// get an random index between 0-25
+// get an random index between 0-25
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText= alphabet ;
+    //  set background color
+    setBackgroundColorById(alphabet);
+
+    
+}
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
